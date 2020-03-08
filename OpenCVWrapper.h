1dbe3bec9d40f8e2802a3638fee0620fc60744c4
Rufus Vijayaratnam <rufusvijayaratnam@gmail.com>
2020-03-08 22:56:57 +0000

    Declarations

---
+++ OpenCVWrapper.h
@@ -0,0 +1,25 @@
+//
+//  OpenCVWrapper.h
+//  CleverCamera
+//
+//  Created by Rufus Vijayaratnam on 28/02/2020.
+//  Copyright © 2020 Rufus Vijayaratnam. All rights reserved.
+//
+
+
+// Need this ifdef, so the C++ header won't confuse Swift
+
+
+#import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
+
+
+
+
+@interface OpenCVWrapper : NSObject
+-(id)initWithImageView:(UIImageView*)iv;
+-(void)startCamera;
+-(void)stopCamera;
+-(void)doBlueTooth;
+@end
+
