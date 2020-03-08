b2c7f829ffa91224ae09e294bc4909442e55e838
Rufus Vijayaratnam <rufusvijayaratnam@gmail.com>
2020-03-08 22:55:28 +0000

    Is still default -- though I added print() to confirm when app has finished launching. Should be removed later.

--- CleverCamera/AppDelegate.swift
+++ CleverCamera/AppDelegate.swift
@@ -12,15 +12,19 @@ import CoreData
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
-
+    
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        print("Application did finish launching")
         return true
     }
 
     // MARK: UISceneSession Lifecycle
-
+    var orientationLock = UIInterfaceOrientationMask.portrait
+    func application(_ application: UIApplication, supportedInterfaceOrientationsFor window: UIWindow?) -> UIInterfaceOrientationMask {
+            return self.orientationLock
+    }
     func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
         // Called when a new scene session is being created.
         // Use this method to select a configuration to create the new scene with.
