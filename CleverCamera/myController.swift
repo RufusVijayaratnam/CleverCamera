1139267d1c3277ccdee61671ab73eb1808271867
Rufus Vijayaratnam <rufusvijayaratnam@gmail.com>
2020-03-11 14:56:36 +0000

    added perspective matrix stuff, and removed the stupid timer stuff

--- CleverCamera/myController.swift
+++ CleverCamera/myController.swift
@@ -88,33 +88,94 @@ public class myController: UIViewController, AVCaptureVideoDataOutputSampleBuffe
         // Send position to BLE Shield (if service exists and is connected)
         if let bleService = btDiscoverySharedInstance.bleService {
             bleService.writePosition(position)
-           // lastPosition = position;
-            
-            // Start delay timer
-            allowTX = false
-            if timerTXDelay == nil {
-                timerTXDelay = Timer.scheduledTimer(timeInterval: 0.1, target: self, selector: #selector(myController.timerTXDelayElapsed), userInfo: nil, repeats: false)
-            }
+           
         }
     }
-    
+
+    func matrixOperation(pixelU: Float, pixelV: Float) {
+        let rotationX = makeRotationMatrix(angle: degToRadians(30), type: 0)
+        let rotationY = makeRotationMatrix(angle: degToRadians(0), type: 1)
+        let rotationZ = makeRotationMatrix(angle: degToRadians(24.5), type: 2)
+        
+        let rotationMatrix: simd_float4x4 = rotationX * rotationZ
+        
+        var rows: [simd_float4]!
+        
+        var tx, ty, tz: Float
+        tx = 0
+        ty = 1990
+        tz = 508
+        
+        rows = [
+            simd_float4(1, 0, 0, tx),
+            simd_float4(0, 1, 0, ty),
+            simd_float4(0, 0, 1, tz),
+            simd_float4(0, 0, 0, 1)
+        ]
+        
+        let translationMatrix = float4x4(rows: rows)
+        
+        let intrinsicCameraRows = [
+            simd_float4(1.2355943168554138e+03, 0, 7.4613134664012841e+02, 0),
+            simd_float4(0,1.2348377141935975e+03, 5.6902027250972412e+02, 0),
+            simd_float4(0, 0, 1, 0),
+            simd_float4(0, 0, 0, 1)
+        ]
+        
+        let intrinsicCameraMatrix = float4x4(rows: intrinsicCameraRows)
+        
+        let transformationMatrix = translationMatrix * rotationMatrix
+        
+        let pixelVector  = simd_float4(pixelU, pixelV, 1, 1)
+        
+        let solutionVector = simd_mul((intrinsicCameraMatrix * transformationMatrix).inverse, pixelVector)
+        
+      //  let position: [UInt16] = [UInt16(solutionVector[0]), UInt16(solutionVector[1])]
+        print("3D mapped with x: \(solutionVector[0]) and y: \(solutionVector[1])")
+        
+    }
+
+    func degToRadians(_ angle: Float) -> Float {
+        return (angle * .pi) / 180
+    }
+
+
+    func makeRotationMatrix(angle: Float, type: Int) -> simd_float4x4 { // 0 for about x, 1 for y, 2 for z
+           var rows: [simd_float4]!
+           switch type {
+           case 0:
+               rows = [
+                   simd_float4(1,              0,                  0, 0),
+                   simd_float4(0, cos(angle), -sin(angle), 0),
+                   simd_float4(0, sin(angle), cos(angle), 0),
+                   simd_float4(0,              0,                0, 1)
+               ]
+           case 1:
+               rows = [
+                   simd_float4( cos(angle), 0, sin(angle), 0),
+                   simd_float4(                0, 1,               0, 0),
+                   simd_float4( -sin(angle), 0, cos(angle), 0),
+                   simd_float4(0,              0,                0, 1)
+               ]
+               
+           case 2:
+               rows = [
+                   simd_float4( cos(angle), -sin(angle), 0, 0),
+                   simd_float4(sin(angle), cos(angle), 0, 0),
+                   simd_float4( 0,          0,          1, 0),
+                   simd_float4(0,              0,                0, 1)
+               ]
+           default:
+               print("Dude its gotta be 0, 1, or 2. Okay? Fucking idiot.")
+               print("Now we have to return identity matrix, and no one wants that")
+               return matrix_identity_float4x4
+           }
+           
+           return float4x4(rows: rows)
+           
+       }
 
     
-    @objc func timerTXDelayElapsed() {
-        self.allowTX = true
-        self.stopTimerTXDelay()
-        print("for some reason this weird timer elapsed thing is running")
-        // Send current slider position
-       // self.sendPosition(UInt16(blueToothSendCount))
-    }
-    
-    func stopTimerTXDelay() {
-        if self.timerTXDelay == nil {
-            return
-        }
-        
-        timerTXDelay?.invalidate()
-        self.timerTXDelay = nil
-    }
+   
 }
 
