9702eafafd4342be6f4a70718aaa061423c019ac
Rufus Vijayaratnam <rufusvijayaratnam@gmail.com>
2020-03-17 14:20:25 +0000

    Added a killIan() function

--- Arduino/sketch_mar17a/sketch_mar17a.ino
+++ Arduino/sketch_mar17a/sketch_mar17a.ino
@@ -1,9 +1,14 @@
 void setup() {
   // put your setup code here, to run once:
-
+  //Ian lost the raspery pi camera
+  killIan();
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
 
+}
+
+void killIan() {
+  
 }
